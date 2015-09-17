28bb3314bd632d3dd78b95121fead7a115daf06b
jimutt <jiut0001@tfe-a411-16.ad.umu.se>
2015-09-17 12:28:14 +0200

    js-lib update to latest

--- Jimmys libs/joystick/joystick.c
+++ Jimmys libs/joystick/joystick.c
@@ -1,21 +1,22 @@
 #include "joystick.h"
 
-void joystick_init(Joystick *js, uint8_t deadzone_x, uint8_t deadzone_y, uint8_t middle_x, uint8_t middle_y) {
-	js->deadzone_x = deadzone_x;
-	js->deadzone_y = deadzone_y;
-	js->middle_x = middle_x;
-	js->middle_y = middle_y;
+void joystick_init(Joystick *js, uint8_t deadzone_left, uint8_t deadzone_right, uint8_t middle_left, uint8_t middle_right) {
+	js->deadzone_left = deadzone_left;
+	js->deadzone_right = deadzone_right;
+	js->middle_left = middle_left;
+	js->middle_right = middle_right;
 
 	//------ Config Timer Interrupt	------
+	/*
 	TCCR1B |= (1 << WGM12); 					// Enable CTC mode.
 	TIMSK1 |= (1 << OCIE1A);					// Enable timer CTC interrupt.
 	
 	OCR1A = 60000; 								// Set compare value.
 	TCCR1B |= ((1 << CS10) | (1 << CS11)); 		// Start timer with 64 prescaler.
-	
+	*/
 	//----------- Config ADC -------------
-	ADCSRA |= (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);  	// ADC prescaler to 128 (gives 125kHz with 16MHz cpu).
-	ADMUX |= (1 << REFS0); 									// Use AVCC as reference.
+	ADCSRA |= (1 << ADPS2) | (1 << ADPS1);  	// ADC prescaler to 128 (gives 125kHz with 8MHz cpu).
+	ADMUX |= (1 << REFS0); 						// Use AVCC as reference.
 	
 	ADCSRA |= (1 << ADEN); 						// Enable ADC
 	ADCSRA |= (1 << ADSC);  					// Start conversion
@@ -23,79 +24,42 @@ void joystick_init(Joystick *js, uint8_t deadzone_x, uint8_t deadzone_y, uint8_t
 }
 
 void joystick_update(Joystick *js) {
-	js->x = joystick_read_axis(JOYSTICK_X_CHANNEL);
-	js->y = joystick_read_axis(JOYSTICK_Y_CHANNEL);
+	//js->x = joystick_read_axis(JOYSTICK_X_CHANNEL);
+	//js->y = joystick_read_axis(JOYSTICK_Y_CHANNEL);
 }
 
-uint8_t joystick_read_axis(uint8_t ch) {
+uint8_t joystick_get_throttle(Joystick *js, uint8_t ch) {
 	ADMUX &= 0xF0;						//Clear the old channel
 	ADMUX |= ch;
 	ADCSRA |= (1<<ADSC);                //Start new conversion
 	while(ADCSRA & (1<<ADSC));          //Wait until the conversion is done
-	return (ADCW>>2);					//Returns the ADC value from 0 - 256
-}
-
-
-uint8_t joystick_get_throttle_dir_combined(Joystick *js, unsigned char motor) {
+	ADCSRA |= (1<<ADSC);                //Start new conversion
+	while(ADCSRA & (1<<ADSC));          //Wait until the conversion is done
 	
-	uint8_t throttle = 0;
-	uint8_t center_x_high = js->middle_x+((float)js->deadzone_x/(float)2);
-	uint8_t center_x_low = js->middle_x-((float)js->deadzone_x/(float)2);
-	uint8_t center_y_high = js->middle_y+((float)js->deadzone_y/(float)2);
-	uint8_t center_y_low = js->middle_y-((float)js->deadzone_y/(float)2);
+	uint8_t mid_min = 127;
+	uint8_t mid_max = 127;
 	
-	uint8_t x = joystick_read_axis(JOYSTICK_X_CHANNEL);
-	uint8_t y = joystick_read_axis(JOYSTICK_Y_CHANNEL);
-	
-	//uint8_t x = js->x;		//Only for debug
-	//uint8_t y = js->y;		//Only for debug		
-
-	//If only turning (no throttle)
-	if(y >= center_y_low && y <= center_y_high){
-		if(x >= center_x_low && x <= center_x_high) {
-			throttle = 127;
-		}
-		else {
-			if(motor == 'l') {
-				throttle = x;
-				} else if(motor == 'r') {
-				throttle = 128-(x-128);
-			}
-		}
+	if(ch == JOYSTICK_LEFT_CHANNEL) {
+		mid_min = js->middle_left - js->deadzone_left;
+		mid_max = js->middle_left + js->deadzone_left;
 	}
-		
-	// If turning right.
-	else if(x > center_x_high) {	
-		throttle = y;
-					
-		if(motor == 'r') {
-			if(y < center_y_low) {
-				throttle = (uint8_t) (y+(((float)(x-128)/(255-y))*128) + 0.5);
-			}
-			else if(y > center_y_high) {
-				throttle = (uint8_t) (y-(((float)(x-128)/y)*128) + 0.5);
-			}
-		}
+	else if(ch == JOYSTICK_RIGHT_CHANNEL) {
+		mid_min = js->middle_right - js->deadzone_right;
+		mid_max = js->middle_right + js->deadzone_right;		
 	}
-		
-	// If turning left.
-	else if(x < center_x_low) {
-		throttle = y;
-		
-		if(motor == 'l') {			
-			if(y < center_y_low) {
-				throttle = (uint8_t) (y+(((float)((255-x)-128)/(255-y))*128) + 0.5);  
-			}
-			else if(y > center_y_high) {
-				throttle = (uint8_t) (y-(((float)(128-x)/y)*128) + 0.5);
-			}
-		}
-	}	
 	
-	else {
-		throttle = y;
-	}	
+	uint8_t th = (ADCW>>2);
+	
+	if(th >= mid_min && th <= mid_max) {
+		th = 127;
+	}
+	
+	else if (th < mid_min) {
+		th = 127-(((float)(mid_min - th)/(float)(mid_min)) * 127);
+	}
+	else if (th > mid_max) {
+		th = (((float)(th-mid_max)/(float)(255-mid_max)) * 127) + 127;
+	}
 
-		
-	return throttle;
+	return th;
 }
